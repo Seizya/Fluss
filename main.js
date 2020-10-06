5d52a3f6389c77f89512cff5c522a4bde3f483e0
八雲 正邪 <Yakumo.Seizya@gmail.com>
2020-10-07 02:55:52 +0900

    Design Base

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ export {
 }
 from "./Seifuncs/main.js";
 //-Events----------------------------------------
+class Memo extends Map {
+    constructor(arg) {
+        super();
+        this.parent = arg.parent;
+        this.children = arg.children;
+        this.title = arg.title;
+        this.detail = arg.detail;
+    }
+
+    bear() {}
+
+    die() {}
+};
